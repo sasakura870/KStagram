e0debb9104f5c01f64796f84cae44980d53d84e0
kdog0625 <kdoooog34@gmail.com>
2020-03-31 00:29:56 +0900

    修正します

--- app/assets/stylesheets/bbaa_files/576a401280f86df0af750746251ef9de567908ff.en.js
+++
@@ -1,3 +0,0 @@
-
-window['cmsg']['assets'] = window['cmsg']['assets'] || {};
-window['cmsg']['assets']["en"] = Object.assign(window['cmsg']['assets']["en"] || {}, {"YLOs":{"js":["01c31d362ec422c24dc815137857d95f2fcec557.strings.js"],"css":[]}});
