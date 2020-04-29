9c3015a280038f8bc38f80dfda3ddfe95e8b4a32
kdog0625 <kdoooog34@gmail.com>
2020-04-29 20:34:37 +0900

    ビュー修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'tweets/index'
   root "tweets#index"
-  get 'about', to: 'static_pages#about'
+  # get 'about', to: 'static_pages#about'
   
   resources :users do
     collection do
